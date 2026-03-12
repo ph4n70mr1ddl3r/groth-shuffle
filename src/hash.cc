0c359c0e9bdf162c551f1fa92347dc0f809a6686
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2026-03-12 19:19:25 +0800

    Optimize Hash::Update(Point): use std::array instead of std::vector
    Replace heap-allocated vector with stack-allocated array for fixed-size
    buffer. Remove unused <vector> include.

--- src/hash.cc
+++ src/hash.cc
@@ -3,7 +3,6 @@
 #include <array>
 #include <cstring>
 #include <stdexcept>
-#include <vector>
 
 static const uint64_t keccakf_rndc[24] = {
     0x0000000000000001ULL, 0x0000000000008082ULL, 0x800000000000808aULL,
@@ -112,7 +111,7 @@ shf::Hash& shf::Hash::Update(const uint8_t* bytes, std::size_t nbytes) {
 }
 
 shf::Hash& shf::Hash::Update(const shf::Point& point) {
-  std::vector<uint8_t> data(Point::ByteSize());
+  std::array<uint8_t, Point::ByteSize()> data{};
   point.Write(data.data());
   Update(data.data(), Point::ByteSize());
   return *this;
